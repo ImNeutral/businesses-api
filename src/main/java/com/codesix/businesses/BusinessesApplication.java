5d938c5a9905bc71ffea36f00c6e86ee2d7c22df
Christian Garillo <garillo.cl@gmail.com>
2021-01-18 20:55:56 +0800

    Initialize project.

---
+++ src/main/java/com/codesix/businesses/BusinessesApplication.java
@@ -0,0 +1,13 @@
+package com.codesix.businesses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codesix/businesses/BusinessesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesix.businesses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
